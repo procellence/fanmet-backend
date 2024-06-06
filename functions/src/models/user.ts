7afd5867ae5a781f9368e82c3e81ff781843dd57
Akshoy Duya <akshoy@implemented.today>
2024-06-06 09:59:24 +0530

    fixed

--- functions/src/models/user.ts
+++ functions/src/models/user.ts
@@ -15,8 +15,29 @@ export interface User extends ModelBase {
   balance: number;
   gender: GENDER_TYPE;
   pictureUrl: string
+  withdrawalOption?: WithdrawalOption;
 }
 
+export interface WithdrawalOption {
+  type: WITHDRAWAL_TYPE;
+  bankWithdrawal?: BankWithdrawal;
+  upiWithdrawal?: UpiWithdrawal;
+}
+
+export interface BankWithdrawal {
+  accountNumber: string;
+  ifscCode: string;
+  accountHolderName: string;
+}
+
+export interface UpiWithdrawal {
+  upiId: string;
+}
+
+enum WITHDRAWAL_TYPE {
+  BANK = 'bank',
+  UPI = 'upi',
+}
 
 enum GENDER_TYPE {
   MALE = 'male',
